c8c26c9556c4775958a3d9e1e28da225f4f04682
youlimsongs <giuhki@gmail.com>
2024-06-14 20:00:15 +0900

    프로젝트 최종 코드 commit

--- src/app/(afterLogin)/_component/LogoutButton.tsx
+++ src/app/(afterLogin)/_component/LogoutButton.tsx
@@ -1,22 +1,48 @@
 "use client"
-import  style from "./logoutButton.module.css";
-export default function LogoutButton() {
-    const me = {
-        id: 'yourim',
-        nickname: '송유림',
-        image: '/5Udwvqim.jpg'
-    }
-    const  onLogout = () => {};
 
-    return(
-        <button className={style.logOutButton} onClick={onLogout}>
-            <div className={style.logOutUserImage}>
-                <img src={me.image} alt={me.id}/>
-            </div>
-            <div className={style.logOutUserName}>
-                <div>{me.nickname}</div>
-                <div>@{me.id}</div>
-            </div>
-        </button>
-    )
+import style from "./logoutButton.module.css";
+import {signOut} from "next-auth/react";
+import {useRouter} from "next/navigation";
+import {Session} from "@auth/core/types";
+import {useQueryClient} from "@tanstack/react-query";
+
+type Props = {
+  me: Session | null
+}
+export default function LogoutButton({ me }: Props) {
+  const router = useRouter();
+  const queryClient = useQueryClient();
+
+  const onLogout = () => {
+    queryClient.invalidateQueries({
+      queryKey: ["posts"],
+    });
+    queryClient.invalidateQueries({
+      queryKey: ["users"],
+    });
+    signOut({ redirect: false })
+      .then(() => {
+        fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/logout`, {
+          method: 'post',
+          credentials: 'include',
+        });
+        router.replace('/');
+      });
+  };
+
+  if (!me?.user) {
+    return null;
+  }
+
+  return (
+    <button className={style.logOutButton} onClick={onLogout}>
+      <div className={style.logOutUserImage}>
+        <img src={me.user?.image as string} alt={me.user?.email as string}/>
+      </div>
+      <div className={style.logOutUserName}>
+        <div>{me.user?.name}</div>
+        <div>@{me.user?.email}</div>
+      </div>
+    </button>
+  )
 }
